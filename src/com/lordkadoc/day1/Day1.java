ded8a188859626fe2f653584c7d712e4f4eca6b3
lucas <lucas.mourado@buawei.com>
2020-12-02 10:05:07 +0100

    first 2 days

---
+++ src/com/lordkadoc/day1/Day1.java
@@ -0,0 +1,61 @@
+package com.lordkadoc.day1;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day1 {
+	
+	public static void main(String[] args) throws IOException {
+		File f = new File("resources/star1.txt");
+		List<Integer> nbs = Files.readAllLines(f.toPath())
+				.stream().map(Integer::valueOf).collect(Collectors.toList());
+		
+		Integer result1 = star1(nbs);
+		System.out.println("Result star1 = " + result1);
+		
+		Integer result2 = star2(nbs);
+		System.out.println("Result star2 = " + result2);
+		
+	}
+	
+	private static int star1(List<Integer> nbs) {
+		for(int i=0;i<nbs.size();i++) {
+			int current = nbs.get(i);
+			for(int j=0;j<nbs.size();j++) {
+				if(i==j)
+					continue;
+				int other = nbs.get(j);
+				if(current + other == 2020) {
+					System.out.println(current + " / " + other);
+					return current * other;
+				}
+			}
+		}
+		return 0;
+	}
+	
+	private static int star2(List<Integer> nbs) {
+		for(int i=0;i<nbs.size();i++) {
+			int a = nbs.get(i);
+			for(int j=0;j<nbs.size();j++) {
+				if(i==j)
+					continue;
+				int b = nbs.get(j);
+				for(int k=0;k<nbs.size();k++) {
+					if(i==k || j == k)
+						continue;
+					int c = nbs.get(k);
+					if(a + b + c == 2020) {
+						System.out.println(a + " / " + b + " / " + c);
+						return a * b * c;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+
+}
---
+++ src/com/lordkadoc/day2/Day2.java
@@ -0,0 +1,56 @@
+package com.lordkadoc.day2;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class Day2 {
+	
+	public static void main(String[] args) throws IOException {
+		File f = new File("resources/star3.txt");
+		List<String> passwords = Files.readAllLines(f.toPath());
+		printStar3Result(passwords);
+		printStar4Result(passwords);
+	}
+	
+	private static void printStar3Result(List<String> passwords) {
+		long validCount = passwords.stream().filter(Day2::isValidPassword).count();
+		System.out.println("Result for star 3 is " + validCount);
+	}
+	
+	private static void printStar4Result(List<String> passwords) {
+		long validCount = passwords.stream().filter(Day2::isValidWithNewPolicy).count();
+		System.out.println("Result for star 4 is " + validCount);
+	}
+
+	private static boolean isValidPassword(String password) {
+		String[] split = password.split(" ");
+		String[] interval = split[0].split("-");
+		int min = Integer.parseInt(interval[0]);
+		int max = Integer.parseInt(interval[1]);
+		char c = split[1].charAt(0);
+		int count = count(c, split[2]);
+		return count >= min && count <= max;
+	}
+
+	private static int count(char c, String s) {
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == c)
+				count++;
+		}
+		return count;
+	}
+	
+	private static boolean isValidWithNewPolicy(String password) {
+		String[] split = password.split(" ");
+		String[] interval = split[0].split("-");
+		String s = split[2];
+		char a = s.charAt(Integer.parseInt(interval[0]) - 1);
+		int b = s.charAt(Integer.parseInt(interval[1]) - 1);
+		char c = split[1].charAt(0);
+		return a == c ^ b == c;
+	}
+	
+}
